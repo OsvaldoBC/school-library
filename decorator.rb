a51da8ed478a745d55d87c6d3ee837edf54e4379
Osvaldo Barrios Cortina <osvaldo.barrios@gmail.com>
2023-03-08 09:34:33 -0500

    Add class called decorator from nameable

---
+++ decorator.rb
@@ -0,0 +1,13 @@
+require './nameable'
+
+# class decorator
+class Decorator < Nameable
+  def initialize(nameable)
+    @nameable = nameable
+    super()
+  end
+
+  def correct_name
+    super()
+  end
+end

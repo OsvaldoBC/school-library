b96820824ae4a2a1b3bf96041763210bb4fa9c11
Osvaldo Barrios Cortina <osvaldo.barrios@gmail.com>
2023-03-10 08:03:23 -0500

    add menu navigation methods

--- app.rb
+++ app.rb
@@ -1,18 +1,36 @@
 # class app
 class App
-    def run
-      menu
-    end
-  
-    def menu
-      puts 'Please choose an option by entering a number:'
-      puts '1 - List all books'
-      puts '2 - List all people'
-      puts '3 - Create a person'
-      puts '4 - Create a book'
-      puts '5 - Create a rental'
-      puts '6 - List all rentals for a given person id'
-      puts '7 - Exit'
+  def run
+    menuoption = gets.chomp.to_i
+    case option
+    when 1
+      list_books
+    when 2
+      list_people
+    when 3
+      create_person
+    when 4
+      create_book
+    when 5
+      create_rental
+    when 6
+      list_rentals_by_person_id
+    when 7
+      puts 'Thank you for using this app!'
+      return
+    else
+      puts 'That is not a valid option'
     end
   end
-  
+
+  def menu
+    puts 'Please choose an option by entering a number:'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
+  end
+end
